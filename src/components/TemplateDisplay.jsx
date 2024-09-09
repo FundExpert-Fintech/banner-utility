0d1cd34b0f8cde0c1b95e6106e18087108d4b557
Harsh <harsh.mundhra@fundexpertfintech.com>
2024-09-09 11:07:35 +0000

    Fix: Message when no templates

--- src/components/TemplateDisplay.jsx
+++ src/components/TemplateDisplay.jsx
@@ -10,9 +10,14 @@ export default function TemplateDisplay({ templates, openModal, showLoader }) {
   return (
     <>
       <div className="flex-1">
-        <h2 className="text-2xl font-bold mb-4">{templates.length > 0 ? 'Generated Templates' : 'Generating Templates...'}
-          <p className='text-sm font-light'>To share this image, please download it first.</p>
-        </h2>
+      {templates.length > 0 
+        ? <h2 className="text-2xl font-bold mb-4"> Generated Templates
+            <p className='text-sm font-light'>To share this image, please download it first.</p>
+          </h2> 
+        : <h2 className="text-2xl font-bold mb-4"> No templates Found!
+            <p className='text-sm font-light'>Please contact our support team for more information</p>
+          </h2> 
+      }
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           { templates.map((template, index) => (
               <Card key={index + 1} className="w-full">
